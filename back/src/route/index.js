b12f5ad5d544890606abd1b19206c6ee6210dbef
Yurii Polevich <polevichx@gmail.com>
2024-07-14 11:39:17 +0300

    to fix

--- back/src/route/index.js
+++ back/src/route/index.js
@@ -3,16 +3,19 @@ const express = require('express')
 const router = express.Router()
 
 // Підключіть файли роутів
-// const test = require('./test')
+const post = require('./post')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-// router.use('/', test)
+router.use('/', post)
 // Використовуйте інші файли роутів, якщо є
 
 router.get('/', (req, res) => {
-  res.status(200).json('Hello World')
+	res.status(200).json('Hello World')
 })
 
 // Експортуємо глобальний роутер
 module.exports = router
+
+
+
